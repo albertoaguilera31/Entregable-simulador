5e3573ed1dfa689f9892e08f1f34b76569296733
albertoaguilera31 <123708306+albertoaguilera31@users.noreply.github.com>
2023-05-20 15:29:53 -0400

    Se realizaron cambio en el html para incluir parrafos descriptivos del simulador, se cambió el texto en las cadenas de texto del archivo js y se corrigieron error en el código para hacerlo más corto y legible para la entrega final

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let bienvenida= alert("¡Bienvenido al Box de crossfit Warriors, el mejor lugar para entrenar el cuerpo y la mente de un guerrero!");
+let bienvenida= alert("¡Bienvenido al Box de crossfit Warriors! El mejor lugar para entrenar el cuerpo y la mente de un guerrero.");
 
-let inicio= prompt("¿Deseas iniciar el camino del guerro o dejar pasar la oportunidad?. Escribe 'iniciar' para continuar o 'salir' para abandonar.");
+let inicio= prompt("¿Deseas iniciar el camino para convertirte en un guerrero o dejar pasar la oportunidad para luego? Escribe 'iniciar' para continuar o 'salir' para abandonar.");
 
 while (inicio !== "iniciar" && inicio !== "salir") {
     inicio = prompt("Respuesta inválida. Por favor, responde iniciar o salir.");
@@ -15,18 +15,16 @@ if(inicio==="iniciar"){
 
         alert(`¡Hola ${nombreUsuario} ${apellidoUsuario}! Aquí inicia tu transformación en un guerrero!`);
     
-        let seleccionUsuario =prompt("¿Qué te gustaria hacer: registrarte para 2 clases gratuitas o registrarte con 50% de descuento durante 6 meses? Ingresa la palabra 'gratis' para las clases gratuitas o la palabra 'descuento' para registrarte con 50% menos.");
+        let seleccionUsuario =prompt("¿Qué te gustaria hacer: registrarte para 2 clases gratuitas o registrarte con 50% de descuento durante los primeros 6 meses? Ingresa la palabra 'gratis' para las clases gratuitas o la palabra 'descuento' para registrarte con 50% menos.");
     
         while (seleccionUsuario !== "gratis" && seleccionUsuario !== "descuento") {
             seleccionUsuario = prompt("Respuesta inválida. Por favor, responde gratis o descuento.");
         }
 
         if(seleccionUsuario ==="gratis"){
-            alert("Felicidades, ya cuentas con 2 clases gratis para empezar tu entrenamiento");
-        }else if(seleccionUsuario==="descuento"){
-            alert("¡Felicidades! Ya obtuviste 50% de descuento en tu matricula");
+            alert("¡Felicidades, ya cuentas con 2 clases gratis para empezar tu entrenamiento con nosotros!");
         }else{
-            alert("¡Debes realizar una elección para continuar!");
+            alert("¡Felicidades! Ya obtuviste 50% de descuento en tu mensualidad durante 6 meses.");
         }
     }
     
@@ -40,19 +38,19 @@ if(inicio==="iniciar"){
         }
     
         if (respuestaCliente === "si") {
-            let respuestaConcurso = parseInt(prompt("¿Cuántos discos de 5 kilos se necesitan para armar una pesa de 60 kilos? Elige cuidadosamente, solo tienes una oportunidad. Tienes 4 opciones: a)8, b)10, c)12, d)14."));
+            let respuestaConcurso = parseInt(prompt("¿Cuántos discos de 5 kilos se necesitan para armar una pesa de 60 kilos? Elige cuidadosamente, solo tienes una oportunidad. Tienes 4 opciones: \n a.-8 \nb.-10 \nc.-12 \nd.-14"));
             switch(respuestaConcurso){
                 case 8:
-                    alert("Respuesta incorrecta, gracias por participar. Suerte para los próximos concursos que tendremos!");
+                    alert("Respuesta incorrecta, gracias por participar. ¡Suerte para los próximos concursos que tendremos!");
                     break;
                 case 10:
-                    alert("Respuesta incorrecta, gracias por participar. Suerte para los próximos concursos que tendremos!");
+                    alert("Respuesta incorrecta, gracias por participar. ¡Suerte para los próximos concursos que tendremos!");
                     break;
                 case 12:
-                    alert("Felicidades! Has ganado el premio prometido! Ahora cuentas con todos los implementos para iniciar tu entrenamiento!");
+                    alert("¡Felicidades! Has ganado tu combo de vestimenta y suplementos deportivos. ¡Ahora cuentas con todos los implementos para iniciar tu entrenamiento!");
                     break;
                 case 14:
-                    alert("Respuesta incorrecta, gracias por participar. Suerte para los próximos concursos que tendremos!");
+                    alert("Respuesta incorrecta, gracias por participar. ¡Suerte para los próximos concursos que tendremos!");
                     break;
                     default:
                     alert("Lo sentimos, eligiste una opción fuera de las opciones disponibles, has perdido tu oportunidad.")
@@ -66,7 +64,7 @@ if(inicio==="iniciar"){
     concursoOpcional();
 
 }else{
-    alert("¡Regresa pronto! Te estaremos esperando para dar inicio a tu transformación");
+    alert("¡Regresa pronto! Te estaremos esperando para dar inicio a tu transformación.");
 }
 
 
